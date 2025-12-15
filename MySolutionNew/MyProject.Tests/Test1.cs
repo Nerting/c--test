38a277783899b4c23852981a88dc7789515c900e
Denis <denelets1@gmail.com>
2025-12-15 03:11:58 +0300

    commit1

---
+++ MySolutionNew/MyProject.Tests/Test1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+[TestClass]
+public class MathTests
+{
+    [TestMethod]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        var result = MathOperations.Add(2.5, 3.5);
+        Assert.AreEqual(6.0, result);
+    }
+    
+    
+    [TestMethod]
+    public void Divide_ShouldReturnCorrectResult()
+    {
+       
+        double a = 10.0;
+        double b = 2.0;
+        double expected = 5.0;
+        
+       
+        double result = MathOperations.Divide(a, b);
+        
+        
+        Assert.AreEqual(expected, result);
+    }
+    
+    
+}
---
+++ MySolutionNew/MyProject/Class1.cs
@@ -0,0 +1,14 @@
+﻿public static class MathOperations
+{
+    public static double Add(double a, double b) => a + b;
+    
+    
+    public static double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Деление на ноль недопустимо.");
+        }
+        return a / b;
+    }
+}
